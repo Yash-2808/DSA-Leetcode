7cb3dcfde9f5e0df6b31dd89a0ad964a00e49ddf
Yash Srivastava <yashsrivastava717@gmail.com>
2026-07-30 22:43:56 +0530

    Time: 1 ms (31.12%) | Memory: 42.1 MB (96.31%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        String s=Integer.toBinaryString(n);
+        int count=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='1'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
