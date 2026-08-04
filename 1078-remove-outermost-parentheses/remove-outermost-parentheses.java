cb5d1f8bdfe5a1b0ef3d359085a69f80ca60d030
Yash Srivastava <yashsrivastava717@gmail.com>
2026-08-04 10:14:00 +0530

    Time: 2 ms (99.70%) | Memory: 43.3 MB (84.09%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -5,7 +5,6 @@ class Solution {
         char[] c = s.toCharArray();
         StringBuilder newString = new StringBuilder();
         int open = 1;
-        int openLeft = 0;
         for (int i = 1; i < len; i++) {
             if (c[i] == '(') {
                 open++;
