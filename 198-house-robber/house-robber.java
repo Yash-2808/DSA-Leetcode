6ebcd5d5028a74a0c8fedb78b1346fc320ee1f9f
Yash Srivastava <yashsrivastava717@gmail.com>
2026-08-21 11:01:30 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.75%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -5,7 +5,7 @@ class Solution {
        
         dp[0]=0;
         dp[1]=nums[0];
-        for(int i=2;i<n+1;i++){
+        for(int i=2;i<=n;i++){
             int pick=nums[i-1]+dp[i-2];
             int noPick=dp[i-1];
             dp[i]=Math.max(pick,noPick);
